43dffcc4c57f23920119aa35455e0ecd4299b434
FabianPortillo1996 <Fabianportillo97@gmail.com>
2018-02-14 23:55:46 -0500

    Cambio de metodos abstractos y propiedad conn

--- src/core/BDAbstract.java
+++ src/core/BDAbstract.java
@@ -12,21 +12,24 @@ public abstract class BDAbstract {
     private final String url= "jdbc:mysql://localhost:3306/chat";
     private final String usuario = "root";
     private final String password = "";
-    private Connection conn;
     
+    protected Connection conn;
     protected String sentencia;
     protected PreparedStatement ps;
     
-    abstract protected void insertar();
-    abstract protected void seleccionar();
     
-    private void crearConexion(){
+    
+    abstract protected void insertar(Object obj);
+    abstract protected void seleccionar(Object obj);
+    
+    private Connection crearConexion(){
         try {
             Class.forName(driver);
             conn = DriverManager.getConnection(url, usuario, password);
         } catch (ClassNotFoundException | SQLException e) {
             System.out.println("Error creando conexion : " + e);
         }
+        return conn;
     }
     
     private void cerrarConexion(){
@@ -36,7 +39,8 @@ public abstract class BDAbstract {
             System.out.println("Error cerrando conexion : " +e);
         }
     }
-    private void ejecutarSentencia(){
+    
+    protected void ejecutarSentencia(){
         try {
             ps.executeUpdate();
         } catch (Exception e) {
