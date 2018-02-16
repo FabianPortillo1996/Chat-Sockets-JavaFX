a9c51321b219be6f41b7008aec77587a35ead2a0
FabianPortillo1996 <Fabianportillo97@gmail.com>
2018-02-15 23:59:19 -0500

    Agregado metod para ejecutar sentencias select

--- src/core/BDAbstract.java
+++ src/core/BDAbstract.java
@@ -1,28 +1,60 @@
 package core;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Fabian
  */
 public abstract class BDAbstract {
-    
+
     private final String driver = "com.mysql.jdbc.Driver";
-    private final String url= "jdbc:mysql://localhost:3306/chat";
+    private final String url = "jdbc:mysql://localhost:3306/chat";
     private final String usuario = "root";
     private final String password = "";
-    
-    protected Connection conn;
+    private Connection conn;
+
     protected String sentencia;
     protected PreparedStatement ps;
-    
-    
-    
-    abstract protected void insertar(Object obj);
-    abstract protected void seleccionar(Object obj);
-    
-    private Connection crearConexion(){
+    protected ResultSet rs;
+    protected int filas;
+
+    abstract protected boolean insertar(Object obj);
+
+    abstract protected Object seleccionar(Object obj);
+
+    /**
+     * Ejecuta una sentencia SQL Preparada
+     *
+     *
+     */
+    protected void ejecutarSentencia() {
+        try {
+            filas = ps.executeUpdate();
+            ps.close();
+            conn.close();
+        } catch (SQLException e) {
+
+        }
+    }
+
+    protected ResultSet ejecutarSelect() {
+        try {
+            rs = ps.executeQuery();
+        } catch (SQLException ex) {
+            
+        }
+        return rs;
+    }
+
+    /**
+     * Crea la conexion ala base de datos y la retorna
+     *
+     * @return Connection conn
+     */
+    protected Connection crearConexion() {
         try {
             Class.forName(driver);
             conn = DriverManager.getConnection(url, usuario, password);
@@ -31,19 +63,5 @@ public abstract class BDAbstract {
         }
         return conn;
     }
-    
-    private void cerrarConexion(){
-        try {
-            conn.close();
-        } catch (Exception e) {
-            System.out.println("Error cerrando conexion : " +e);
-        }
-    }
-    
-    protected void ejecutarSentencia(){
-        try {
-            ps.executeUpdate();
-        } catch (Exception e) {
-        }
-    }
+
 }
